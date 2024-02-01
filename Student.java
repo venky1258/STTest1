95d594a039bd3a8ac78407f609dc17883b0e4301
venky1258 <140711690+venky1258@users.noreply.github.com>
2024-02-01 18:58:06 +0530

    Add files via upload
    Test1

---
+++ Student.java
@@ -0,0 +1,18 @@
+package com.example1.ST1;
+
+public class Student {
+	public String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void displayInfo()
+	{
+		System.out.print("Your name is "+name);
+	}
+
+}
